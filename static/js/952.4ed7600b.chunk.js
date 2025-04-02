73c4eb7ff3aa314015b7bfaf49deb77a1f8e88ba
Audrey Clanton <aclan3916@gmail.com>
2025-04-02 14:34:24 -0400

    Updates

---
+++ static/js/952.4ed7600b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_running_log=self.webpackChunkreact_running_log||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.4ed7600b.chunk.js.map
